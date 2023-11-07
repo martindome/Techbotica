241d73099988e422225f64e02070ec630d0703a7
Martin Dome <martindome96@gmail.com>
2023-11-07 11:33:05 -0300

    script automatico para base de datos remota

--- SQLFiles/CreateDatabaseAzure.sql
+++ SQLFiles/CreateDatabaseAzure.sql
@@ -22,6 +22,20 @@ exec (@sql)
  PRINT @sql
 end
 
+declare @procName varchar(500)
+declare cur cursor 
+
+for select [name] from sys.objects where type = 'p'
+open cur
+fetch next from cur into @procName
+while @@fetch_status = 0
+begin
+    exec('drop procedure [' + @procName + ']')
+    fetch next from cur into @procName
+end
+close cur
+deallocate cur
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
